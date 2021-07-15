77bc339d1aa6a36163c2927257b667aa40a6d690
marilantagliari <marilantagliari@gmail.com>
2021-07-15 18:42:54 -0300

    trying AT commands

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,5 @@
-/**************************************************************
- *
- * For this example, you need to install PubSubClient library:
- *   https://github.com/knolleary/pubsubclient
- *   or from http://librarymanager/all#PubSubClient
- *
- * TinyGSM Getting Started guide:
- *   https://tiny.cc/tinygsm-readme
- *
- * For more MQTT examples, see PubSubClient library
- *
- **************************************************************
- * This example connects to HiveMQ's showcase broker.
- *
- * You can quickly test sending and receiving messages from the HiveMQ webclient
- * available at http://www.hivemq.com/demos/websocket-client/.
- *
- * Subscribe to the topic GsmClientTest/ledStatus
- * Publish "toggle" to the topic GsmClientTest/led and the LED on your board
- * should toggle and you should see a new message published to
- * GsmClientTest/ledStatus with the newest LED status.
- *
- **************************************************************/
 
-// Select your modem:
-// #define TINY_GSM_MODEM_SIM800
-// #define TINY_GSM_MODEM_SIM808
-// #define TINY_GSM_MODEM_SIM868
-// #define TINY_GSM_MODEM_SIM900
 #define TINY_GSM_MODEM_SIM7000
-// #define TINY_GSM_MODEM_SIM7000SSL
-// #define TINY_GSM_MODEM_SIM7080
-// #define TINY_GSM_MODEM_SIM5360
-// #define TINY_GSM_MODEM_SIM7600
-// #define TINY_GSM_MODEM_UBLOX
-// #define TINY_GSM_MODEM_SARAR4
-// #define TINY_GSM_MODEM_M95
-// #define TINY_GSM_MODEM_BG96
-// #define TINY_GSM_MODEM_A6
-// #define TINY_GSM_MODEM_A7
-// #define TINY_GSM_MODEM_M590
-// #define TINY_GSM_MODEM_MC60
-// #define TINY_GSM_MODEM_MC60E
-// #define TINY_GSM_MODEM_ESP8266
-// #define TINY_GSM_MODEM_XBEE
-// #define TINY_GSM_MODEM_SEQUANS_MONARCH
 
 // Set serial for debug console (to the Serial Monitor, default speed 115200)
 #define SerialMon Serial
@@ -59,54 +15,34 @@
 SoftwareSerial SerialAT(2, 3); // RX, TX
 #endif
 
-// See all AT commands, if wanted
-// #define DUMP_AT_COMMANDS
-
 // Define the serial console for debug prints, if needed
 #define TINY_GSM_DEBUG SerialMon
-
-// Range to attempt to autobaud
-// NOTE:  DO NOT AUTOBAUD in production code.  Once you've established
-// communication, set a fixed baud rate using modem.setBaud(#).
-#define GSM_AUTOBAUD_MIN 9600
-#define GSM_AUTOBAUD_MAX 115200
 
 // Add a reception delay, if needed.
 // This may be needed for a fast processor at a slow baud rate.
 // #define TINY_GSM_YIELD() { delay(2); }
 
-// Define how you're planning to connect to the internet.
-// This is only needed for this example, not in other code.
-#define TINY_GSM_USE_GPRS true
-#define TINY_GSM_USE_WIFI false
-
 // set GSM PIN, if any
 #define GSM_PIN ""
+#define MODEM_RST 5
+#define MODEM_PWKEY 4
+#define MODEM_DTR 25
+
+#define MODEM_TX 26
+#define MODEM_RX 27
 
 // Your GPRS credentials, if any
 const char apn[] = "";
 const char gprsUser[] = "";
 const char gprsPass[] = "";
 
-// Your WiFi connection credentials, if applicable
-const char wifiSSID[] = "YourSSID";
-const char wifiPass[] = "YourWiFiPass";
-
 #include <TinyGsmClient.h>
+#include <root_cert.h>
 
-// Just in case someone defined the wrong thing..
-#if TINY_GSM_USE_GPRS && not defined TINY_GSM_MODEM_HAS_GPRS
-#undef TINY_GSM_USE_GPRS
-#undef TINY_GSM_USE_WIFI
-#define TINY_GSM_USE_GPRS false
-#define TINY_GSM_USE_WIFI true
-#endif
-#if TINY_GSM_USE_WIFI && not defined TINY_GSM_MODEM_HAS_WIFI
-#undef TINY_GSM_USE_GPRS
-#undef TINY_GSM_USE_WIFI
-#define TINY_GSM_USE_GPRS true
-#define TINY_GSM_USE_WIFI false
-#endif
+#define CERT_FILE_ROOT_PEM "C:\\Users\\mano\\apps_studio\\rastreador\\trackerTest\\include\\roots.pem"
+#define CERT_FILE_ROOT_PRIMARY_CRT "C:\\Users\\mano\\apps_studio\\rastreador\\trackerTest\\include\\gtsltsr_primary.crt"
+#define CERT_FILE_THING_PRIV_PEM "C:\\Users\\mano\\apps_studio\\rastreador\\trackerTest\\include\\esp32-device_2.pem"
+#define CERT_FILE_THING_PUB_PEM "C:\\Users\\mano\\apps_studio\\rastreador\\trackerTest\\include\\esp32-device-public_2.pem"
 
 #define DUMP_AT_COMMANDS
 
@@ -119,77 +55,189 @@ TinyGsm modem(SerialAT);
 #endif
 TinyGsmClient client(modem);
 
-uint32_t lastReconnectAttempt = 0;
-
 void setup()
 {
     // Set console baud rate
     SerialMon.begin(115200);
     delay(10);
 
-    // !!!!!!!!!!!
-    // Set your reset, enable, power pins here
-    // !!!!!!!!!!!
-
     SerialMon.println("Wait...");
 
     // Set GSM module baud rate
-    TinyGsmAutoBaud(SerialAT, GSM_AUTOBAUD_MIN, GSM_AUTOBAUD_MAX);
-    // SerialAT.begin(9600);
-    delay(6000);
 
     // Restart takes quite some time
     // To skip it, call init() instead of restart()
-    SerialMon.println("Initializing modem...");
+    SerialMon.println("Set pins power reset modem...");
+
+    pinMode(MODEM_PWKEY, OUTPUT);
+    digitalWrite(MODEM_PWKEY, HIGH);
+    delay(10);
+    digitalWrite(MODEM_PWKEY, LOW);
+    delay(1010); //Ton 1sec
+    digitalWrite(MODEM_PWKEY, HIGH);
+
+    pinMode(MODEM_RST, OUTPUT);
+    digitalWrite(MODEM_RST, LOW);
+    delay(260); //Treset 252ms
+    digitalWrite(MODEM_RST, HIGH);
+    delay(5000);
+
+    SerialMon.println("Start serial AT modem...");
+    SerialAT.begin(9600, SERIAL_8N1, MODEM_TX, MODEM_RX);
+    delay(6000);
+
+    SerialMon.println("Init / reset modem...");
+    modem.init();
     modem.restart();
-    // modem.init();
+
+    delay(10000);
+
+    SerialMon.println("Waiting testAt...");
+    for (int8_t i = 0; i < 100; i++) //timeout 100 x 100ms = 10sec
+    {
+        if (modem.testAT())
+        {
+            //Serial.println("Wait time:%F sec\n", i/10));
+            Serial.printf("Wait time: %d\n", i);
+            break;
+        }
+        Serial.println("waiting modem...");
+    }
+
+    /*
+    AT+CSSLCFG="cipher
+suite",<ctxindex>,<cip
+her_index>,<ciphersuit
+e>
+    0xC02B QAPI_NET_TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256
+
+Response
++CSSLCFG: "sslversion",(0-5),(0-5)
++CSSLCFG: "ciphersuite",(0-5),(0-7),(0x008A,0x008B,0x008C,0x008D,0x00A8,0x00
+A9,0x00AE,0x00AF,0x002F,0x0033,0x0035,0x0039,0xC02A,0xC02B,0x
+C02C,0xC02D,0xC02E,0xC02F,0xC030,0xC031,0xC032,0xC09C,0xC09
+D,0xC09E,0xC09F,0xC0A0,0xC09F,0xC0A1,0xC0A2,0xC0A3,0xCC13,0x
+CC14,0xCC15)
++CSSLCFG: "ignorertctime",(0-5),(0-1)
++CSSLCFG: "protocol",(0-5),(1-2)
++CSSLCFG: "sni",(0-5),<servername>
++CSSLCFG: "ctxindex",(0-5)
++CSSLCFG: "convert",(1-3),(<cname>,[<keyname>[,<passkey>]])
+
+AT+CSSLCFG="sslver
+sion",<ctxindex>,<sslv
+ersion>
+    3 QAPI_NET_SSL_PROTOCOL_TLS_1_2
+
+
+
+    mqtt.2030.ltsapis.goog 8883 or 443
+
+    DNS over TCP or UDP
+
+    
+    AT+CSSLCFG="protoc
+ol",<ctxindex>,<protoc
+ol>
+    Response
+OK
+If failed:
++CME ERROR: <err>
+Parameters
+<ctxindex> (0-5)
+<protocol>
+1 QAPI_NET_SSL_TLS_E
+    */
+    SerialMon.println("--------------------- START TEST AT COMMANDS");
+    modem.sendAT(GF("+CSSLCFG=\"sslversion\",0,3"));
+    if (modem.waitResponse() != 1)
+    {
+        Serial.println("return sslversion fail");
+        return;
+    }
+    modem.sendAT(GF("+SHSSL=1,") CERT_FILE_ROOT_PRIMARY_CRT);
+    if (modem.waitResponse() != 1)
+    {
+        Serial.println("return  fail");
+        return;
+    }
+
+        /*    modem.sendAT("CFSINIT");
+
+    const int cert_size = sizeof(CERT_FILE);
+
+    modem.sendAT(GF("CFSWFILE=0," CERT_FILE ",0,"), cert_size, GF(",5000"));
+    if (modem.waitResponse() != 1)
+    {
+        Serial.println("return CFSWFILE fail");
+        return;
+    }
+    SerialMon.println("--------------------- END TEST AT COMMANDS");
+*/
+
+    /*
+    ////////
+    SerialMon.println("--------------------- START TEST AT COMMANDS - SRGDamia cert file");
+
+    modem.sendAT(GF("FSCREATE=" CERT_FILE));
+    if (modem.waitResponse() != 1)
+        return;
+
+    const int cert_size = sizeof(cert);
+
+    modem.sendAT(GF("FSWRITE=" CERT_FILE ",0,"), cert_size, GF(",10"));
+    if (modem.waitResponse(GF(">")) != 1)
+    {
+        return;
+    }
+
+    for (int i = 0; i < cert_size; i++)
+    {
+        char c = pgm_read_byte(&cert[i]);
+        modem.stream.write(c);
+    }
+
+    modem.stream.write(GSM_NL);
+    modem.stream.flush();
+
+    if (modem.waitResponse(2000) != 1)
+        return;
+
+    modem.sendAT(GF("+SSLSETCERT=\"" CERT_FILE "\""));
+    if (modem.waitResponse() != 1)
+        return;
+    if (modem.waitResponse(5000L, GF(GSM_NL "+SSLSETCERT:")) != 1)
+        return;
+    const int retCode = modem.stream.readStringUntil('\n').toInt();
+
+    SerialMon.println();
+    SerialMon.println();
+    SerialMon.println(F("****************************"));
+    SerialMon.print(F("Setting Certificate: "));
+    SerialMon.println((0 == retCode) ? "OK" : "FAILED");
+    SerialMon.println(F("****************************"));
+
+    SerialMon.println("--------------------- END TEST AT COMMANDS - SRGDamia cert file");
+*/
+    Serial.print("Waiting for network...");
+    if (!modem.waitForNetwork())
+    {
+        Serial.println(" fail");
+        delay(10000);
+        return;
+    }
+    Serial.println(" success");
+
+    if (modem.isNetworkConnected())
+    {
+        Serial.println("Network connected");
+    }
 
     String modemInfo = modem.getModemInfo();
     SerialMon.print("Modem Info: ");
     SerialMon.println(modemInfo);
 
-#if TINY_GSM_USE_GPRS
-    // Unlock your SIM card with a PIN if needed
-    if (GSM_PIN && modem.getSimStatus() != 3)
-    {
-        modem.simUnlock(GSM_PIN);
-    }
-#endif
-
-#if TINY_GSM_USE_WIFI
-    // Wifi connection parameters must be set before waiting for the network
-    SerialMon.print(F("Setting SSID/password..."));
-    if (!modem.networkConnect(wifiSSID, wifiPass))
-    {
-        SerialMon.println(" fail");
-        delay(10000);
-        return;
-    }
-    SerialMon.println(" success");
-#endif
-
-#if TINY_GSM_USE_GPRS && defined TINY_GSM_MODEM_XBEE
-    // The XBee must run the gprsConnect function BEFORE waiting for network!
-    modem.gprsConnect(apn, gprsUser, gprsPass);
-#endif
-
-    SerialMon.print("Waiting for network...");
-    if (!modem.waitForNetwork())
-    {
-        SerialMon.println(" fail");
-        delay(10000);
-        return;
-    }
-    SerialMon.println(" success");
-
-    if (modem.isNetworkConnected())
-    {
-        SerialMon.println("Network connected");
-    }
-
-#if TINY_GSM_USE_GPRS
-    // GPRS connection parameters are usually set after network registration
-    SerialMon.print(F("Connecting to "));
+    SerialMon.print(F("Connecting to APN: "));
     SerialMon.print(apn);
     if (!modem.gprsConnect(apn, gprsUser, gprsPass))
     {
@@ -201,13 +249,23 @@ void setup()
 
     if (modem.isGprsConnected())
     {
-        SerialMon.println("GPRS connected");
+        SerialMon.println("GPRS connected.. fim");
     }
-#endif
 }
 
 void loop()
 {
+
+    if (SerialAT.available())
+    {
+        SerialMon.write(SerialAT.read());
+    }
+    if (SerialMon.available())
+    {
+        SerialAT.write(SerialMon.read());
+    }
+    delay(0);
+    /*
     // Make sure we're still registered on the network
     if (!modem.isNetworkConnected())
     {
@@ -223,7 +281,6 @@ void loop()
             SerialMon.println("Network re-connected");
         }
 
-#if TINY_GSM_USE_GPRS
         // and make sure GPRS/EPS is still connected
         if (!modem.isGprsConnected())
         {
@@ -241,6 +298,6 @@ void loop()
                 SerialMon.println("GPRS reconnected");
             }
         }
-#endif
     }
+    */
 }
